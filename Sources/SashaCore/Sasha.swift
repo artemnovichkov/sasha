3d5eea9acb79ab7b27760698bfca547b5328bc38
Artem Novichkov <novichkoff93@gmail.com>
2017-08-24 22:15:38 +0600

    Add hardcode basic structure for project

--- Sources/SashaCore/Sasha.swift
+++ Sources/SashaCore/Sasha.swift
@@ -19,22 +19,49 @@ public final class Sasha {
     
     public func run() throws {
         guard arguments.count > 1 else {
-            throw Error.missingFileName
+            throw Error.missingProjectName
         }
-        let fileName = arguments[1]
+        let projectName = arguments[1]
         
         do {
-            try FileSystem().createFile(at: fileName)
+            let projectFolder = try FileSystem().createFolder(at: projectName)
+            let uiFolder = try projectFolder.createSubfolder(named: "UI")
+            try addPlatformSubfolders(to: uiFolder)
+            
+            let uxFolder = try projectFolder.createSubfolder(named: "UX")
+            try addPlatformSubfolders(to: uxFolder)
+            
+            let referencesFolder = try projectFolder.createSubfolder(named: "references")
+            try referencesFolder.createSubfolder(named: "main-screens")
+            try referencesFolder.createSubfolder(named: "menu")
+            try referencesFolder.createSubfolder(named: "cards")
+            try referencesFolder.createSubfolder(named: "another-case")
+            
+            let stuffFolder = try projectFolder.createSubfolder(named: "stuff")
+            try stuffFolder.createSubfolder(named: "logos")
+            try stuffFolder.createSubfolder(named: "icons")
+            try stuffFolder.createSubfolder(named: "patterns")
+            try stuffFolder.createSubfolder(named: "stocks")
+            try stuffFolder.createSubfolder(named: "source")
         }
         catch {
             throw Error.failedToCreateFile
         }
     }
+    
+    private func addPlatformSubfolders(to folder: Folder) throws {
+        let iosFolder = try folder.createSubfolder(named: "iOS")
+        try iosFolder.createSubfolder(named: "old")
+        try iosFolder.createSubfolder(named: "png")
+        let androidFolder = try folder.createSubfolder(named: "Android")
+        try androidFolder.createSubfolder(named: "old")
+        try androidFolder.createSubfolder(named: "png")
+    }
 }
 
 public extension Sasha {
     enum Error: Swift.Error {
-        case missingFileName
+        case missingProjectName
         case failedToCreateFile
     }
 }

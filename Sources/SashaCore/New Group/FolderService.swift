82ad67b39a4ec8addea4a1431406c53ae55a4e2e
Artem Novichkov <artem.novichkov@rosberry.com>
2017-08-29 13:30:56 +0600

    Move project structure to config file

---
+++ Sources/SashaCore/New Group/FolderService.swift
@@ -0,0 +1,68 @@
+//
+//  FolderService.swift
+//  Files
+//
+//  Created by Artem Novichkov on 29/08/2017.
+//
+
+import Foundation
+
+final class FolderService {
+    
+    enum Keys {
+        static let slash = "/"
+    }
+    
+    private class Folder {
+        let name: String
+        let level: Int
+        var subfolders = [Folder]()
+        
+        init(name: String, level: Int) {
+            self.name = name
+            self.level = level
+        }
+        
+        func add(_ folder: Folder) {
+            if folder.level == level + 1 {
+                subfolders.append(folder)
+                return
+            }
+            subfolders.last?.add(folder)
+        }
+    }
+    
+    func paths(fromString string: String) -> [String] {
+        let components = string.components(separatedBy: "\n")
+        let allFolders = components.map { component -> Folder in
+            let name = component.replacingOccurrences(of: "-", with: "")
+            let level = component.filter { $0 == "-" }
+            return Folder(name: name, level: level.count)
+        }
+        var paths = [String]()
+        var currentLevel = 0
+        var currentPath: String?
+        allFolders.forEach { folder in
+            if let path = currentPath {
+                if folder.level > currentLevel {
+                    currentPath = path + Keys.slash + folder.name
+                }
+                else {
+                    var components = path.components(separatedBy: Keys.slash)
+                    var subComponents = components[0..<folder.level]
+                    subComponents.append(folder.name)
+                    currentPath = subComponents.joined(separator: Keys.slash)
+                }
+                currentLevel = folder.level
+            }
+            else {
+                currentPath = folder.name
+                currentLevel = folder.level
+            }
+            if let path = currentPath {
+                paths.append(path)
+            }
+        }
+        return paths
+    }
+}
--- Sources/SashaCore/Sasha.swift
+++ Sources/SashaCore/Sasha.swift
@@ -12,6 +12,7 @@ import Files
 public final class Sasha {
     
     private let arguments: [String]
+    private let folderService = FolderService()
     
     public init(arguments: [String] = CommandLine.arguments) {
         self.arguments = arguments
@@ -24,44 +25,24 @@ public final class Sasha {
         let projectName = arguments[1]
         
         do {
-            let projectFolder = try FileSystem().createFolder(at: projectName)
-            let uiFolder = try projectFolder.createSubfolder(named: "UI")
-            try addPlatformSubfolders(to: uiFolder)
+            let projectFile = try FileSystem().currentFolder.file(named: "project.sasha")
+            let projectString = try projectFile.readAsString()
             
-            let uxFolder = try projectFolder.createSubfolder(named: "UX")
-            try addPlatformSubfolders(to: uxFolder)
-            
-            let referencesFolder = try projectFolder.createSubfolder(named: "references")
-            try referencesFolder.createSubfolder(named: "main-screens")
-            try referencesFolder.createSubfolder(named: "menu")
-            try referencesFolder.createSubfolder(named: "cards")
-            try referencesFolder.createSubfolder(named: "another-case")
-            
-            let stuffFolder = try projectFolder.createSubfolder(named: "stuff")
-            try stuffFolder.createSubfolder(named: "logos")
-            try stuffFolder.createSubfolder(named: "icons")
-            try stuffFolder.createSubfolder(named: "patterns")
-            try stuffFolder.createSubfolder(named: "stocks")
-            try stuffFolder.createSubfolder(named: "source")
+            let paths = folderService.paths(fromString: projectString)
+            try paths.forEach { path in
+                let finalPath = projectName + FolderService.Keys.slash + path
+                try FileSystem().createFolder(at: finalPath)
+            }
         }
         catch {
-            throw Error.failedToCreateFile
+            throw Error.failedToCreateFolder
         }
-    }
-    
-    private func addPlatformSubfolders(to folder: Folder) throws {
-        let iosFolder = try folder.createSubfolder(named: "iOS")
-        try iosFolder.createSubfolder(named: "old")
-        try iosFolder.createSubfolder(named: "png")
-        let androidFolder = try folder.createSubfolder(named: "Android")
-        try androidFolder.createSubfolder(named: "old")
-        try androidFolder.createSubfolder(named: "png")
     }
 }
 
 public extension Sasha {
     enum Error: Swift.Error {
         case missingProjectName
-        case failedToCreateFile
+        case failedToCreateFolder
     }
 }

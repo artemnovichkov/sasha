e5675af5625c3426e4a33bc24be3f73494c2db67
Artem Novichkov <artem.novichkov@rosberry.com>
2017-09-20 10:55:23 +0600

    Decrease Swift tool version
    Simple hack for buddybuild to add the project

--- Package.swift
+++ Package.swift
@@ -1,20 +1,16 @@
-// swift-tools-version:4.0
-
 import PackageDescription
 
 let package = Package(
     name: "Sasha",
-    dependencies: [
-        .package(url: "https://github.com/johnsundell/files.git", from: "1.0.0"),
-        .package(url: "https://github.com/nsomar/Swiftline.git", from: "0.0.0"),
-    ],
     targets: [
-        .target(
+        Target(
             name: "Sasha",
-            dependencies: ["SashaCore"]),
-        .target(
-            name: "SashaCore",
-            dependencies: ["Files", "Swiftline"],
-            path: "./Sources/SashaCore"),
+            dependencies: ["SashaCore"]
+        ),
+        Target(name: "SashaCore")
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/johnsundell/files.git", majorVersion: 1),
+        .Package(url: "https://github.com/nsomar/Swiftline.git", majorVersion: 0)
     ]
 )
